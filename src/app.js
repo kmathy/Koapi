073c3977eea607db289f2429c358bbb822c7a291
Kevin MATHY <kmathy@adneom.com>
2017-06-24 21:15:43 +0200

    feat(console): manakin for console output

--- src/app.js
+++ src/app.js
@@ -2,14 +2,19 @@
 require('babel-register')({
     sourceMaps: true
 })
+require('manakin').global // output colors in Development
 import Koa from 'koa'
 import { loadInitializers } from './initializers/_loadInitializers'
 
 const app = new Koa()
 const PORT = 3000
 
+// #### LOAD INITIALIZERS ####
+
 loadInitializers(app)
 
+// #### START APP ####
+
 app.listen(PORT, () => {
-    console.log(`App listening on localhost:${PORT}`)
+    console.info(`App listening on localhost:${PORT}`)
 })
