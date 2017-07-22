f8f0beb1b69af570eade5055839afec836716661
Kevin MATHY <kmathy@adneom.com>
2017-07-22 16:48:09 +0200

    feat(jwt): add jwt token

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -4,11 +4,14 @@ import helmet from 'koa-helmet'
 import helmetOptions from './helmet.options'
 import bodyparser from 'koa-bodyparser'
 import bodyparserOptions from './bodyparser.options'
+import jwt from 'koa-jwt'
+import { options, paths } from './jwt.options'
 
 const middlewares = [
   logger(),
   helmet(helmetOptions),
-  bodyparser(bodyparserOptions)
+  bodyparser(bodyparserOptions),
+  jwt(options).unless(paths)
 ]
 
 export default () => compose(middlewares)
---
+++ src/middlewares/jwt.options.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// options include the shared secret
+
+export const options = {
+  secret: ''
+}
+
+// exclude jwt to check these routes
+
+export const paths = {
+  path: [
+    '/api/users'
+  ]
+}
